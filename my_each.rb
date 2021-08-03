f0bb592b77eb37d1f7bde0d9ca5f4c4a5df01937
Diane Nguyen <diane.nguyen@gmail.com>
2021-08-03 08:58:51 -0700

    My Each assignment completed.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+# def my_each(words) # put argument(s) here
+
+def my_each(words)
+  if block_given?
+    counter = 0  
+    while counter < words.length
+      yield(words[counter])
+      counter += 1
+    end  
+    words
+  end
 end
